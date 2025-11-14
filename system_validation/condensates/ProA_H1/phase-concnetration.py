a94badedf0ff324a26176d41bab75bff26c3a761
WangLiguo-kyrie <97430007+WangLiguo-kyrie@users.noreply.github.com>
2025-11-14 13:48:05 +0100

    Add files via upload

---
+++ system_validation/condensates/ProA_H1/phase-concnetration.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+
+import sys
+import os
+import numpy as np
+
+import pandas as pd
+
+
+intervals=[12000,15000,18000,21000,24000,27000,30000]
+factor=(20863*15+12203*18)/(26154*15+13986*18)
+data=[]
+with open('phase.txt','w') as g:
+    print('Begin\tEnd\tDiluteconcentration\tDenseconcentration',file=g)
+    for i in range(6):
+        former=intervals[i]
+        latter=intervals[i+1]
+        print('density interval: {} to {}'.format(former,latter))
+
+        filename='density-{}_{}ns.xvg'.format(former,latter)
+        print(filename)
+        os.system("sed -i 's/^@/#/g' %s " %filename)
+        data_interval = np.loadtxt(filename)
+        print(data_interval.shape)
+        #Dilute phase
+        dilute1=data_interval[(data_interval[:,0]>15)]
+        dilute2=data_interval[(data_interval[:,0]<-15)]
+        dilute_conc=(np.mean(dilute1[:,1])+np.mean(dilute2[:,1]))*factor/2
+        #Dense phase
+        dense=data_interval[((data_interval[:,0]>-2)&(data_interval[:,0]<2))]*factor
+        print(dense)
+        dense_conc=(np.mean(dense[:,1]))
+        print('{}\t{}\t{}\t{}'.format(former,latter,dilute_conc,dense_conc),file=g)
+
+
+
